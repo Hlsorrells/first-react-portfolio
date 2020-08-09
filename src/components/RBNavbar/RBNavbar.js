cc735925223d5107e3aedac2a42ba0b50f7b4a93
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-08-09 13:28:19 -0400

    Set alignment for links in navbar

--- src/components/RBNavbar/RBNavbar.js
+++ src/components/RBNavbar/RBNavbar.js
@@ -3,23 +3,29 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './RBNavbar.css';
 
+const styles = {
+    nav: {
+        justifyContent: "flex-end",
+        flexDirection: "row",
+        width: "100%"
+    }
+}
+
 const RBNavbar = () => {
     return (
-        <Navbar expand="lg" className="navbar">
+        <Navbar className="navbar" fixed='top'>
             <Navbar.Brand href="#home" className="navbar-brand">
                 {/* <img
-                    src="/logo.svg"
-                    width="30"
-                    height="30"
+                    src="./logo.svg"
                     className="d-inline-block align-top"
-                    alt="React Bootstrap logo"
+                    alt="logo"
                 /> */}
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="justify-content-end">
+                <Nav style={styles.nav}>
                     <Nav.Item >
-                        <Nav.Link href="/home" className='navbar-link'>Home</Nav.Link>
+                        <Nav.Link href="#home" className='navbar-link'>Home</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link eventKey="/about" className='navbar-link'>About</Nav.Link>
