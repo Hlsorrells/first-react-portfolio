ccc77eab7c7cf5162a986033daa5ac35e4be31d5
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-08-14 12:43:06 -0400

    Updates

---
+++ precache-manifest.1b5b737a096d1d34b6e776b4adfb23f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24160efdabdd8f8928cc8e1420fca91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcee4d48f4d77b93722",
+    "url": "/static/css/main.1a1479e1.chunk.css"
+  },
+  {
+    "revision": "fdde8d0f81c127b41cc4",
+    "url": "/static/js/2.db46ebd2.chunk.js"
+  },
+  {
+    "revision": "954511d31499ca0e6270b821e58f37cb",
+    "url": "/static/js/2.db46ebd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcee4d48f4d77b93722",
+    "url": "/static/js/main.070ead53.chunk.js"
+  },
+  {
+    "revision": "9bf601af7410d4bb6000",
+    "url": "/static/js/runtime-main.c52b6ca0.js"
+  },
+  {
+    "revision": "fd3ecc5492a2f2b706a95d7837c378fe",
+    "url": "/static/media/SchoolSpot.fd3ecc54.gif"
+  },
+  {
+    "revision": "d8d33cdfe7b0a0ab7601b69a2bc1133f",
+    "url": "/static/media/casper-college.d8d33cdf.jpg"
+  },
+  {
+    "revision": "e3adf082eadb35a85a5aed8e443d56bf",
+    "url": "/static/media/drinkin-bad.e3adf082.gif"
+  },
+  {
+    "revision": "432a0add234055cacf36b44ba28b5aa4",
+    "url": "/static/media/heatherSorrellsProfilePic.432a0add.PNG"
+  },
+  {
+    "revision": "224ea6c343d440fa1b56dfb4ba19ba33",
+    "url": "/static/media/logo_white.224ea6c3.svg"
+  },
+  {
+    "revision": "86238473ef480d508101d711c1ff0f87",
+    "url": "/static/media/megabites.86238473.gif"
+  },
+  {
+    "revision": "2f007caef64a9b3c970d0eb9e8a34ab4",
+    "url": "/static/media/uncc.2f007cae.jpeg"
+  }
+]);
